d784774a94b07db14362374fba826c597570bbbd
linlin-cmd <1852351836@qq.com>
2019-09-12 16:49:23 +0800

    hh

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -7,6 +7,17 @@ use Illuminate\Http\Request;
 class EventController extends Controller
 {
     public function event(){
+    	//获取xml
+    	$xml_string = file_get_contents('php://input');
+    	//存放路径
+    	$path =storage_path('logs/wechat/'.date("Y-m-d",time()).".log");
+    	//创建日志
+    	$append =file_put_contents($path,$xml_string,FILE_APPEND);
+    	//追加内容
+    	file_put_contents($path,"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n",FILE_APPEND);
     	
+    	$xml_obj = simplexml_load_string($xml_string,'SimpleXMLElement',LIBXML_NOCDATA);
+    	$xml_arr = (array)$xml_obj;
+        \Log::Info(json_encode($xml_arr,JSON_UNESCAPED_UNICODE));
     }
 }
