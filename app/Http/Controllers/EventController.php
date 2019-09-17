9ac7f6dbc11f12d6907253541229f56b3abcf0d8
linlin-cmd <1852351836@qq.com>
2019-09-17 10:15:26 +0800

    aa

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -11,6 +11,7 @@ class EventController extends Controller
     	$xml_string = file_get_contents('php://input');
     	//存放路径
     	$path =storage_path('logs/wechat/'.date("Y-m-d",time()).".log");
+    	file_put_contents($path,"\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n\n",FILE_APPEND);
     	//创建日志
     	$append =file_put_contents($path,$xml_string,FILE_APPEND);
     	//追加内容
