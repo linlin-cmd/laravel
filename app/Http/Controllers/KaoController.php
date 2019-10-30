6d99d27052a3cb4f235c52b1cd726eabd1dcf208
linlin-cmd <1852351836@qq.com>
2019-10-30 15:56:35 +0800

    aa

--- app/Http/Controllers/KaoController.php
+++ app/Http/Controllers/KaoController.php
@@ -226,6 +226,7 @@ class KaoController extends Controller
         $app_id =request()->app_id;
         $appsecret =request()->appsecret;
         $app_url ="http://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+        dd($app_url);
         if (empty($app_id)){
             return json_encode(['ret'=>201,'msg'=>'请传递app_id'],JSON_UNESCAPED_UNICODE);
         }
